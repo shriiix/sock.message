76da1201f8053583399abfb392b54e50c4eacbf0
shriiix <shrighadi003@gmail.com>
2024-07-26 16:23:41 +0530

    execution of application

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const http = require("http")
 const express = require("express")
-
+const { Server } = require("socket.io");
+const path = require('path');
 const app = express();
 
 const server = http.createServer(app)
-app.use(express.static("/public"))
+
+//io input output
+
+const io = new Server(server) // new server created with S.. and give our server in para
+
+
+io.on('connection', (socket) => { //socket is cliend (user)
+    //console.log('new user connected',socket.id); //id automatically manageable
+    
+    //socket message exchange
+    socket.on("user-message",(message)=>{
+        console.log("A new user message",message)
+        io.emit('message',message);
+    })
+
+
+});
+
+//express for http request 
+app.use(express.static(path.resolve('./public')))
+app.get("/",(req,res)=>{
+    return res.sendFile("/public/index.html")
+})
 
 
 server.listen(9000, ()=> console.log(`server started at port : 9000`))
